d1d8b454dbb19d2c476e3fc350c6446a21495451
Bradford Trevino <bradford.w.trevino@gmail.com>
2022-03-25 16:21:21 -0700

    Day one progress!

--- 2-checkout/server/index.js
+++ 2-checkout/server/index.js
@@ -28,19 +28,21 @@ app.use(express.json());
  *
  */
 app.post('/checkout', (req, res) => {
-  var name = req.body.name;
-  var email = req.body.email;
-  var password = req.body.password;
-  var addline1 = req.body.addline1;
-  var addline2 = req.body.addline2;
-  var city = req.body.city;
-  var state = req.body.state;
-  var zip = req.body.zip;
-  var phoneNum = req.body.phoneNum;
-  var ccNum = req.body.ccNum;
-  var expDate = req.body.expDate;
-  var cvv = req.body.cvv;
-  var billZip = req.body.billZip;
+  var {
+    name,
+    email,
+    password,
+    addline1,
+    addline2,
+    city,
+    state,
+    zip,
+    phoneNum,
+    ccNum,
+    expDate,
+    cvv,
+    billZip
+  } = req.body;
 
   var insert = `INSERT INTO responses (
     name,
